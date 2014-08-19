0b2d827b2b705ddd05f5b13e1df6e4b21061ee00
Steve Nelson <nelsonsteve50@gmail.com>
2014-08-19 08:30:02 -0500

    Final Version August

--- run_analysis.R
+++ run_analysis.R
@@ -6,14 +6,17 @@ Reading in the data
 #reinstall or update some of the packages called here.
 ========================================================
   
-  #the training and test sets are combined creating a data.frame  
-  #with the dimensions 10299 X 561.  The steps are  
-  #1. Rbind the following data files together.
-  #  a. Xtrain and Xtest into a 10299 X 561 data.frame with the data and observations
-  #  b. X_train and y_train into a 10299 X 1 data.frame of raw activity codes  
-  #  c. subject_test and subjecttrain into a 10299 X 1 data.frame of subject ids  
-  #  d. cbind the data.frames together into a 10,299 X 563 data.frame.  
-  #  e. apply activity names to activity codes 
+#the training and test sets are combined creating a data.frame  
+#with the dimensions 10299 X 561.  The steps are  
+#1. Rbind the following data files together.
+#  a. Xtrain and Xtest into a 10299 X 561 data.frame with the data and observations
+#  b. X_train and y_train into a 10299 X 1 data.frame of raw activity codes  
+#  c. subject_test and subjecttrain into a 10299 X 1 data.frame of subject ids  
+#  d. cbind the data.frames together into a 10,299 X 563 data.frame.  
+#  e. combine train and test activity codes together
+#  f. combine train and test subject ids and rbind to data frame
+#  g. apply activity names to activity codes
+ 
   
   
 trainXall=read.table("./UCI HAR Dataset/train/X_train.txt", colClasses="numeric")
@@ -32,49 +35,55 @@ activityname<-read.table("./UCI HAR Dataset/activity_labels.txt")
 complete_rawdata$activity<-factor(complete_rawdata$activity,levels=activityname$V1, labels=activityname$V2 )
 
 
-
-
-
-#Get and scope out variable name list of the data. Result is 561 variable names.
-colfeature<-read.table("./UCI HAR Dataset/features.txt", colClasses="character")
 #Gets the variable name list of the data. Result is 561 variable names.  
 #Subset the activity data to only include "mean()" and "std()" columns as that is one reasonable interpretation of the instructions.  This results in 66 columns
 colfeature<-read.table("./UCI HAR Dataset/features.txt", colClasses="character")
-names <- colfeature[grepl("-mean\\(\\)|-std\\(\\)", colfeature$V2),]
+names(colfeature)<-c("featureid","featurename")
+nameset <- colfeature[grepl("-mean\\(\\)|-std\\(\\)", colfeature$featurename),] 
 
 
 #subset the data on these columns,
-refined_rawdata<-complete_rawdata[,c(562,563,as.numeric(names$V1))] #this also reorders data. 562 is "subject" number; 531 is factor "activity" 
+refined_rawdata<-complete_rawdata[,c(562,563,as.numeric(nameset$featureid))] #this also reorders data. 562 is "subject" number; 531 is factor "activity" 
+
+#Feature names are untidy need clarification. This sequence of 14 steps to clean the names.
+#This creates names with major idendifiers separated by "." , less common
+#identifiers written out and dashes and parens eliminated.
+nameset1<-gsub("BodyBody" ,"Body", nameset$featurename)#second body is assumed a typo.
+nameset2<-gsub("^t", "t.", nameset1)
+nameset3<-gsub("^f", "f.", nameset2)
+nameset4<-gsub("Acc", ".Acc.", nameset3)  
+nameset5<-gsub("Gyro", ".Gyro.", nameset4)
+nameset6<-gsub("Mag", "Magnitude.", nameset5)
+nameset7<-gsub("-", "", nameset6)
+nameset8<-gsub("X",".X", nameset7)
+nameset9<-gsub("Y",".Y", nameset8)
+nameset10<-gsub("Z",".Z", nameset9)
+nameset11<-gsub("*[Ss]td\\(\\)", "Std", nameset10)
+nameset12<-gsub("*[Mm]ean\\(\\)*", "Mean", nameset11)
+nameset13<-gsub("Jerk", "Jerk.", nameset12)
+namesetfinal<-tolower(nameset13)
 
 
-#Names are untidy.  Get rid of some unneeded key strokes. First capitalization.  
-#Then re-capitalize acc and gyr for clarity. Fix apparent text typos
-nameslow<-tolower(names$V2) #lowercase
-names_clean1<-gsub("acc", "Acc", nameslow,fixed=TRUE)#emphasize device
-names_clean2<-gsub("gyr", "Gyr", names_clean1,fixed=TRUE)#emphasize device
-names_clean3<-gsub("bodybody" ,"body", names_clean2)#second body is assumed a typo.
-
-
-
-#apply column names to refined data
-names(refined_rawdata)<-c("subject","activity",names_clean3)
-wearable<-refined_rawdata # add better name
-
+#apply column names to refined data.
+names(refined_rawdata)<-c("subject","activity",namesetfinal)
+activitymonitor<-refined_rawdata # add better data.table name
 
 #archive file as wearable.txt
-write.table(wearable,"wearable.txt",col.names=TRUE, row.names=FALSE)
+#write.table(activitymonitor,"activitymonitor.txt",col.names=TRUE, row.names=FALSE)
 
 
 # create a second tidy set for submission to Coursera with the average of #each variable for each activity and each subject. 
 require(plyr)
 require(dplyr) 
 require(reshape2)
-wearmelt<-melt(wearable, id.var=c("subject","activity"))
+actmonitor<-melt(activitymonitor, id.var=c("subject","activity"))
 # this creates the long format from the wide.
-wearablegroups<-group_by(wearmelt, variable, subject, activity)
+monitorgroups<-group_by(actmonitor, variable, subject, activity)
 #once melted and grouped the dimensins 679734X4 - long format
 #instructions:Creates a second, independent tidy data set with the average of each variable for each activity and each subject. This implies the order of variable, subjectid, activity and mean value.
-tidywearable<-summarize(wearablegroups, average_for_var_by_subj_activity=round(mean(value),4))
+tidyactivitymonitor<-summarize(monitorgroups, average.measure.by.subjects.activity=round(mean(value),4))
 # 11880X4
-write.table(tidywearable,"tidywearable.txt",row.names=FALSE)# without the row.names false argument, the text file columns don't line up right.
-
+# write.table(tidyactivitymonitor,"tidyactivitymonitor.txt",row.names=FALSE)# without the row.names false argument, the text file columns don't line up right.
+#sample data for first subject
+a<-subset(tidyactivitymonitor,tidyactivitymonitor[,2]==1)
+a
